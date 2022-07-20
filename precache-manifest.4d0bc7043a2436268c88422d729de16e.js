2730cbbf75e6aca4d4ec35dd9d036b8a4fd1df8e
Code Sandbox <hello@codesandbox.io>
2022-07-20 15:08:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4d0bc7043a2436268c88422d729de16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0ab4d326b6cc71750bf3520bc144a0",
+    "url": "https://mkr774.github.io/csb-7ggwj9/index.html"
+  },
+  {
+    "revision": "1adae293a7324812d1e4",
+    "url": "https://mkr774.github.io/csb-7ggwj9/static/css/main.5eed18d7.chunk.css"
+  },
+  {
+    "revision": "4b7424c1b5d6336febfe",
+    "url": "https://mkr774.github.io/csb-7ggwj9/static/js/2.4d866460.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https://mkr774.github.io/csb-7ggwj9/static/js/2.4d866460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1adae293a7324812d1e4",
+    "url": "https://mkr774.github.io/csb-7ggwj9/static/js/main.0b7ba9f6.chunk.js"
+  },
+  {
+    "revision": "8eba59269987b079a1a3",
+    "url": "https://mkr774.github.io/csb-7ggwj9/static/js/runtime-main.5586a5e2.js"
+  }
+]);
